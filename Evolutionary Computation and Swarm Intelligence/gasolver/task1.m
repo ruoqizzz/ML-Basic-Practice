7930760c216e483c1bd7d75ee8e8b0d453f34372
Rocky <wsgdrfzv@gmail.com>
2019-04-01 20:07:40 +0200

    finish task 1

--- Evolutionary Computation and Swarm Intelligence/gasolver/task1.m
+++ Evolutionary Computation and Swarm Intelligence/gasolver/task1.m
@@ -1,14 +1,14 @@
 %%
-star = GAparams;
+star = GAparams
 star.objParams.star = star1;
 [best, fit, stat] = GAsolver(2, [0 20 ; 0 20], 'circle', 50, 100, star);
 
-%%
+%% Q6
 press = [2, 1.75, 1.5, 1.25,1];
 best_all = zeros(size(press,2),2);
 fit_all = zeros(size(press,2),1); 
 figure(1)
-for i = 1:size(press,2)
+for i = 1:size(1,2)
     star = GAparams;
     star.objParams.star = star1;
     star.select.func = 'rank';
@@ -19,3 +19,27 @@ for i = 1:size(press,2)
     fit_all(i) = fit;
     subplot(2,3,i),ga_plot_diversity(stat,press(i));
 end
+
+%% Q8 star 2
+star = GAparams;
+star.objParams.star = star2;
+star.visual.active = 1; 
+star.visual.func = 'circle';
+star.select.func = 'rank';
+star.select.pressure = 1.25;
+% star.verbose = false;
+[best, fit, stat] = GAsolver(2, [0 20 ; 0 20], 'circle', 200, 200, star);
+figure(2)
+ga_plot_diversity(stat,press(i));
+
+%% Q8 star 3
+star = GAparams;
+star.objParams.star = star3;
+star.visual.active = 1; 
+star.visual.func = 'circle';
+star.select.func = 'rank';
+star.select.pressure = 1.25;
+% star.verbose = false;
+[best, fit, stat] = GAsolver(2, [0 20 ; 0 20], 'circle', 100, 200, star);
+figure(2)
+ga_plot_diversity(stat,press(i));
